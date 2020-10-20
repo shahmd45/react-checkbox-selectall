d66e2d9224ce3b867968246cc94f9ada60fb7aed
Hussain <md.shaheb.hussain@accenture.com>
2020-10-20 15:58:27 +0530

    create react app barebone setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        New Project to start from strach.
+    </div>
+  );
+}
+
+export default App;
